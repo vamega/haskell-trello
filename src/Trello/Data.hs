7a10046419d4f4f15a219b44b05bd3e95cfaa5c3
Varun Madiath <vamega@gmail.com>
2014-04-05 18:22:29 -0400

    Broken parsing code.

--- src/Trello/Data.hs
+++ src/Trello/Data.hs
@@ -1,15 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Trello.Data where
 
 import Data.Data
 import Data.Time
+import Data.Aeson.Types
+import Control.Applicative
+import Control.Monad
+import Trello.Api
 
-newtype BoardRef      = BoardRef Int deriving (Show, Eq, Ord)
-newtype ListRef       = ListRef Int deriving (Show, Eq, Ord)
-newtype CardRef       = CardRef Int deriving (Show, Eq, Ord)
-newtype MemberRef     = MemberRef Int deriving (Show, Eq, Ord)
-newtype CommentRef    = CommentRef Int deriving (Show, Eq, Ord)
-newtype ChecklistRef  = ChecklistRef Int deriving (Show, Eq, Ord)
-newtype AttachmentRef = AttachmentRef Int deriving (Show, Eq, Ord)
+newtype BoardRef      = BoardRef String deriving (Show, Eq, Ord)
+newtype ListRef       = ListRef String deriving (Show, Eq, Ord)
+newtype CardRef       = CardRef String deriving (Show, Eq, Ord)
+newtype MemberRef     = MemberRef String deriving (Show, Eq, Ord)
+newtype CommentRef    = CommentRef String deriving (Show, Eq, Ord)
+newtype ChecklistRef  = ChecklistRef String deriving (Show, Eq, Ord)
+newtype AttachmentRef = AttachmentRef String deriving (Show, Eq, Ord)
 
 data Board = Board {
    boardRef     :: BoardRef
@@ -76,6 +81,27 @@ data Attachment = Attachment {
   ,attachmentTime :: UTCTime
 } deriving (Show, Eq, Ord)
 
+instance FromJSON MemberRef where
+  parseJSON (String s) = return $ MemberRef s
+
+instance FromJSON BoardRef where
+  parseJSON (String s) = return $ BoardRef s
+
+instance FromJSON ListRef where
+  parseJSON (String s) = return $ ListRef s
+
+instance FromJSON CardRef where
+  parseJSON (String s) = return $ CardRef s
+
+instance FromJSON CommentRef where
+  parseJSON (String s) = return $ CommentRef s
+
+instance FromJSON ChecklistRef where
+  parseJSON (String s) = return $ ChecklistRef s
+
+instance FromJSON AttachmentRef where
+  parseJSON (String s) = return $ AttachmentRef s
+
 instance FromJSON Card where
   parseJSON (Object o) =
     Card <$> o .: "id"
@@ -83,8 +109,9 @@ instance FromJSON Card where
          <*> o .: "idList"
          <*> o .: "name"
          <*> o .: "desc"
-         <*> o .; "idMembers"
-         <*> o ?.: "due"
-         <*> o .: ""
+         <*> o .: "idMembers"
+         <*> liftM parseTimestamp (o .:? "due")
+         <*> liftM parseTimestamp (o .: "dateLastActivity")
+         <*> o .: "closed" -- Confirm this mapping later.
   parseJSON _          = fail "Can't decode"
 
