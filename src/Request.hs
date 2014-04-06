78a33f8f028f1cb5c031910edb6cee65eec76297
JJ <jj@example.com>
2014-04-06 10:18:37 -0400

    merge

--- src/Request.hs
+++ src/Request.hs
@@ -3,24 +3,32 @@ module Request where
 import Network.HTTP.Conduit
 import Trello.Data
 import Network.HTTP.Base
+import Control.Monad.IO.Class (MonadIO)
+import qualified Data.ByteString.Lazy.Char8 as B
 
-BOARD_URL = "https://api.trello.com/1/boards/"
+instance FromJSON Board where
+     parseJSON (Object v) = Board <$>
+                            v .: "name" <*>
+                            v .: "age"
+     -- A non-Object value is of the wrong type, so fail.
+     parseJSON _          = mzero
 
-test_id = 4eea4ffc91e31d1746000046
-test_params = [("lists", "open"),
-          ("list_fields", "name"),
-          ("fields", "name,desc"),
-          ("key", "0cc879616fe466ceb58bd6ee6fc73b8b"),
-          ("token", "[optional_auth_token]")]
+board_url = "https://api.trello.com/1/boards/"
+
+test_id = "4d5ea62fd76aa1136000000c"
+test_params = [("lists", "all"),
+               ("members", "all"),
+               ("list_fields", "name"),
+               ("fields", "name,desc"),
+               ("key", "0cc879616fe466ceb58bd6ee6fc73b8b")]
+               --("token", "[optional_auth_token]")]
 
 type HttpParams = [(String, String)]
 
-getBoard :: Int -> HttpParams -> Maybe Board
-getBoardURL boardId params = BOARD_URL ++ boardId ++ "?" ++ urlEncodeVars
-
+getBoard :: MonadIO m => String -> HttpParams -> m B.ByteString
+getBoard boardId params = simpleHttp requestURL
+  where requestURL = board_url ++ boardId ++ "?" ++ (urlEncodeVars params)
 
 main = do
-  putStrLn $ getBoardURL test_id test_params
-  --simpleHttp "http://www.example.com/foo.txt" >>= L.writeFile "foo.txt"
-
---simpleHTTP (getRequest "http://www.haskell.org/") >>= fmap (take 100) . getResponseBody
+  board <- getBoard test_id test_params
+  B.putStrLn board
